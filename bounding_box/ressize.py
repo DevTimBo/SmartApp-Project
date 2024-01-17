50182f99fbacd8687296e3a9cfbd4d59e0a2faad
alhafg <79312407+alhafg@users.noreply.github.com>
2024-01-17 14:15:47 +0100

    add functions to adjust the size of the bboxes

--- bounding_box/ressize.py
+++ bounding_box/ressize.py
@@ -4,38 +4,97 @@ import os
 
 from config import YOLO_HEIGHT, YOLO_WIDTH
 
-def get_image_size(image):
-    height= image.shape[0]
-    width = image.shape[1]
-    return width, height
+
+def calculate_bbox_scale_factor(old_bbox, new_bbox):
+    old_bbox = old_bbox[(len(old_bbox) - 1)]
+
+    old_width = old_bbox[2] - old_bbox[0]
+    old_height = old_bbox[3] - old_bbox[1]
+
+    new_width = new_bbox[2] - new_bbox[0]
+    new_height = new_bbox[3] - new_bbox[1]
+
+    width_scale_factor = new_width / old_width
+    height_scale_factor = new_height / old_height
+
+    return width_scale_factor, height_scale_factor
+
+
+def scale_box(new_image_width, new_image_height, box):
+    width_scale = new_image_width / 640
+    height_scale = new_image_height / 640
+    xmin, ymin, xmax, ymax = box
+    # Bbox-Koordinaten anpassen
+    new_xmin = int(xmin * width_scale)
+    new_ymin = int(ymin * height_scale)
+    new_xmax = int(xmax * width_scale)
+    new_ymax = int(ymax * height_scale)
+
+    return new_xmin, new_ymin, new_xmax, new_ymax
+
+
+def resize_imaged_without_expand_dim(image, width, height):
+    resized_image = cv2.resize(image, (width, height))
+    return resized_image
+
+
+def cut_links_bbox(verkleinerungsfaktor, bbox):
+    return bbox[0] + (bbox[2] - bbox[0]) * (1 - verkleinerungsfaktor)
+
+
+def cut_top_bbox(verkleinerungsfaktor, bbox):
+    return bbox[1] + (bbox[3] - bbox[1]) * verkleinerungsfaktor
+
+
+def add_bottom_bbox(vergroesserungsfaktor, bbox):
+    return bbox[3] + (bbox[3] - bbox[1]) * vergroesserungsfaktor
+
+
+def add_rechts_bbox(vergroesserungsfaktor, bbox):
+    return bbox[2] + (bbox[2] - bbox[0]) * vergroesserungsfaktor
+
+
+def add_links_bbox(vergroesserungsfaktor, bbox):
+    return bbox[0] - (bbox[2] - bbox[0]) * vergroesserungsfaktor
+
 
 def resize_image(image_path, image_width, image_height):
     image = cv2.imread(image_path)
     resized_image = cv2.resize(image, (YOLO_WIDTH, YOLO_HEIGHT))
     resized_image = np.expand_dims(resized_image, axis=0)
-    return resized_image 
+    return resized_image
+
+
+def get_image_size(image):
+    height = image.shape[0]
+    width = image.shape[1]
+    return width, height
+
 
 def resize(input_path, output_path, width, height):
-    image =  cv2.imread(input_path)
+    image = cv2.imread(input_path)
     resized_image = cv2.resize(image, (width, height))
-    cv2.imwrite(output_path, resized_image)    
+    cv2.imwrite(output_path, resized_image)
+
 
 def get_width_height_shape(image_path):
-    image =  cv2.imread(image_path)
+    image = cv2.imread(image_path)
     height_ratio = YOLO_HEIGHT / image.shape[0]
     width_ratio = YOLO_WIDTH / image.shape[1]
     return width_ratio, height_ratio
 
+
 def scale_bounding_box(bounding_boxes, width_ratio, height_ratio):
     scalde_bounding_boxes = []
-    
+
     for box in bounding_boxes:
-        x_min = np.round(box[0]*width_ratio,2)
-        y_min = np.round(box[1]*height_ratio,2)
-        x_max = np.round(box[2]*width_ratio,2)
-        y_max = np.round(box[3]*height_ratio,2)
+        x_min = np.round(box[0] * width_ratio, 2)
+        y_min = np.round(box[1] * height_ratio, 2)
+        x_max = np.round(box[2] * width_ratio, 2)
+        y_max = np.round(box[3] * height_ratio, 2)
         scalde_bounding_boxes.append([x_min, y_min, x_max, y_max])
     return scalde_bounding_boxes
+
 
 def create_directories(output_path_images, output_path_annotations, new_width, new_height):
     output_path_images = output_path_images + '/' + str(new_height) + 'x' + str(new_width)
