c3bf5938a9700139b8f9a248ab0a8c08cb137a86
lenilol <emil.hillebrand@gmx.de>
2024-01-25 15:53:35 +0100

    first Api integration

---
+++ android_app/tfliteapp/lib/api.flask.dart
@@ -0,0 +1,49 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:http/http.dart' as http;
+
+var fileName = "";
+
+//base64?
+Future data(File image) async {
+  var timestamp = DateTime.now().toString();
+  var url = 'http://10.0.2.2:5000/upload';
+  var uri = Uri.parse(url);
+  var request = http.MultipartRequest("POST", uri);
+
+  Map<String, String> headers = {"Content-type": "multipart/form-data"};
+
+  fileName = image.path.split('/').last;
+  //request.fields["inputPicture"] = "inputPicture" + timestamp;
+  request.files.add(
+    http.MultipartFile(
+      'files[]',
+      image.readAsBytes().asStream(),
+      image.lengthSync(),
+      filename: fileName,
+    ),
+  );
+  request.headers.addAll(headers);
+  var res = await request.send();
+
+  http.Response response = await http.Response.fromStream(res);
+
+  //http.Response response = await http.get(uri);
+  var data = response.body;
+  var decodedData = jsonDecode(data);
+  return (decodedData['message'].toString());
+  //return response.toString();
+}
+
+Future getData() async {
+  var url = 'http://10.0.2.2:5000/get-prediction-flutter/' + fileName;
+  var uri = Uri.parse(url);
+
+  http.Response response = await http.get(uri);
+  var data = response.body;
+  var decodedData = jsonDecode(data);
+  return (decodedData['flutterpred'].toString());
+  //return response.toString();
+}
