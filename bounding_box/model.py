c3e89d49de535318952fbc3b85af64de8be86553
alhafg <79312407+alhafg@users.noreply.github.com>
2024-01-26 22:27:54 +0100

    adjust plot and prediction function

--- bounding_box/model.py
+++ bounding_box/model.py
@@ -10,7 +10,7 @@ from bounding_box.ressize import resize_image, get_width_height_shape, scale_bou
     scale_box, \
     resize_imaged_without_expand_dim, cut_links_bbox, cut_top_bbox, add_bottom_bbox, add_rechts_bbox, add_links_bbox, \
     get_position_difference_between_boxes, adjust_position_of_the_boxes, get_center_of_box, calculate_new_position, \
-    get_difference_center_of_boxes, cut_rechts_bbox
+    get_difference_center_of_boxes, cut_rechts_bbox, scale_bounding_one_box
 
 from bounding_box.config import LEARNING_RATE, GLOBAL_CLIPNORM, NUM_CLASSES_ALL, SUB_BBOX_DETECTOR_MODEL, BBOX_PATH, \
     MAIN_BBOX_DETECTOR_MODEL, class_ids, main_class_ids, sub_class_ids, YOLO_WIDTH, YOLO_HEIGHT
@@ -43,6 +43,7 @@ def get_org_ms_boxes_for_pred(best_predicted_class, org_ms_boxes_person, org_ms_
     else:
         pass
     return pred_box, class_name
+
 
 
 def get_templated_data(boxes, confidence, classes, org_ms_boxes_person, org_ms_boxes_wohnsitz, org_ms_boxes_ausbildung,
@@ -457,7 +458,7 @@ def edit_sub_boxes_cut_left_or_top(ausbildung, person, wohnsitz, wwa):
 
 def plot_image(image, ausbildung, person, wohnsitz, wwa, best_predicted):
     image = cv2.imread(image)
-    image = resize_imaged_without_expand_dim(image, YOLO_WIDTH, YOLO_HEIGHT)
+    # image = resize_imaged_without_expand_dim(image, YOLO_WIDTH, YOLO_HEIGHT)
     fig, ax = plt.subplots(1)
     ax.imshow(image)
     #
@@ -500,11 +501,11 @@ def predict_image(image, model):
     resized_image = resize_image(image, YOLO_WIDTH, YOLO_HEIGHT)
     predictions = model.predict(resized_image)
     boxes = predictions['boxes']
-    boxes = scale_bounding_box(boxes, ratios[0], ratios[1])
+    # boxes = scale_bounding_box(boxes, ratios[0], ratios[1])
     confidence = predictions['confidence']
     classes = predictions['classes']
 
-    return boxes, confidence, classes
+    return boxes, confidence, classes, ratios
 
 
 def define_model(num_classes):
