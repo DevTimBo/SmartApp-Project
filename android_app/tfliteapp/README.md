c2dd9404482291d620bdec94eb871ce9fef81723
lenilol <emil.hillebrand@gmx.de>
2024-02-13 15:14:05 +0100

    small changes for better looks

--- android_app/tfliteapp/README.md
+++ android_app/tfliteapp/README.md
@@ -7,9 +7,16 @@ A small project to show tf lite can be working in Flutter. To create an APK you 
 After some research I found out why my projekt isnt working. The [tflite](https://pub.dev/packages/tflite) plugin isn't working no more you need to use the [tflite_v2](https://pub.dev/packages/tflite_v2) to make it work. 
 
 ## compile the android App:
-1. go in to the projekt folder  
-2. ```bash 
-    flutter build apk --split-per-abi
+1. go in to the projekt folder 
+    ```bash 
+         cd android_app/tfliteapp/
+    ```
+2. Install pakages and create APKs
+     ```bash 
+        flutter pub get 
+    ```
+    ```bash 
+        flutter build apk --split-per-abi
     ```
 3. now you got three apks in build/app/outputs/flutter-apk
 4. use the apk for your device architecture 
--- android_app/tfliteapp/lib/main.dart
+++ android_app/tfliteapp/lib/main.dart
@@ -7,35 +7,31 @@ import 'package:flutter/material.dart';
 // import 'package:tflite_v2/tflite_v2.dart';
 import 'package:flutter_pdfview/flutter_pdfview.dart';
 
-//camera view parts from: https://docs.flutter.dev/cookbook/plugins/picture-using-camera
+//inspired by: https://docs.flutter.dev/cookbook/plugins/picture-using-camera
 //Modified
 
 String _classifications = "try again";
 String? _image;
 
 Future<void> main() async {
-  // Ensure that plugin services are initialized so that `availableCameras()`
-  // can be called before `runApp()`
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Obtain a list of the available cameras on the device.
+  // get available Cameras
   final cameras = await availableCameras();
 
-  // Get a specific camera from the list of available cameras.
+  // Get a first Camera (experience. back Camera)
   final firstCamera = cameras.first;
 
   runApp(
     MaterialApp(
       theme: ThemeData.dark(),
       home: TakePictureScreen(
-        // Pass the appropriate camera to the TakePictureScreen widget.
         camera: firstCamera,
       ),
     ),
   );
 }
 
-// A screen that allows users to take a picture using a given camera.
+// screen to preview and take pictures
 class TakePictureScreen extends StatefulWidget {
   const TakePictureScreen({
     super.key,
@@ -55,33 +51,33 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   @override
   void initState() {
     super.initState();
-    //load tfLiteModel
-    _loadModel().then((_) {});
+    //load tfLiteModel (not used old functions for TFLite functionality)
+    //_loadModel().then((_) {});
     // To display the current output from the Camera,
-    // create a CameraController.
+    // create the CameraController.
     _controller = CameraController(
-      // Get a specific camera from the list of available cameras.
+      // Get the choosen camera
       widget.camera,
-      // Define the resolution to use.
+      // resolution of Camera
       ResolutionPreset.medium,
     );
 
-    // Next, initialize the controller. This returns a Future.
+    // initialize controller
     _initializeControllerFuture = _controller.initialize();
   }
 
   @override
   void dispose() {
-    // Dispose of the controller when the widget is disposed.
     _controller.dispose();
     super.dispose();
   }
 
-  _loadModel() async {
-    // for the tfLite prototype
-    //await Tflite.loadModel(
-    //    model: 'assets/faceId.tflite', labels: 'assets/labels.txt');
-  }
+  // old function used for TFLite
+  //_loadModel() async {
+  // for the tfLite prototype
+  //await Tflite.loadModel(
+  //    model: 'assets/faceId.tflite', labels: 'assets/labels.txt');
+  //}
 
   _predict(File image) async {
     // for the tfLite model now used for the API call
@@ -115,49 +111,40 @@ class TakePictureScreenState extends State<TakePictureScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Take a picture')),
-      // You must wait until the controller is initialized before displaying the
-      // camera preview. Use a FutureBuilder to display a loading spinner until the
-      // controller has finished initializing.
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
-            // If the Future is complete, display the preview.
+            // show preview
             return CameraPreview(_controller);
           } else {
-            // Otherwise, display a loading indicator.
+            // show loading
             return const Center(child: CircularProgressIndicator());
           }
         },
       ),
+      // button for taking picture
       floatingActionButton: FloatingActionButton(
-        // Provide an onPressed callback.
         onPressed: () async {
-          // Take the Picture in a try / catch block. If anything goes wrong,
-          // catch the error.
+          // taking the picture
           try {
-            // Ensure that the camera is initialized.
             await _initializeControllerFuture;
 
-            // Attempt to take a picture and get the file `image`
-            // where it was saved.
             final image = await _controller.takePicture();
             final imageAsFile = File(image.path);
+            // send and get the prediction from the API
             await _predict(imageAsFile);
             if (!mounted) return;
-
-            // If the picture was taken, display it on a new screen.
+            // Displaying the PDF on new screen
             await Navigator.of(context).push(
               MaterialPageRoute(
-                builder: (context) => DisplayPictureScreen(
-                  // Pass the automatically generated path to
-                  // the DisplayPictureScreen widget.
+                builder: (context) => DisplayPDFScreen(
                   imagePath: image.path,
                 ),
               ),
             );
           } catch (e) {
-            // If an error occurs, log the error to the console.
+            // error gets logged
             print(e);
           }
         },
@@ -168,10 +155,10 @@ class TakePictureScreenState extends State<TakePictureScreen> {
 }
 
 // A widget that displays the PDF taken by the user.
-class DisplayPictureScreen extends StatelessWidget {
+class DisplayPDFScreen extends StatelessWidget {
   final String imagePath;
 
-  const DisplayPictureScreen({super.key, required this.imagePath});
+  const DisplayPDFScreen({super.key, required this.imagePath});
 
   @override
   Widget build(BuildContext context) {
